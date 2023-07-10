4f24aec163b2f12c9e48f395a70b487208b72304
Alexander Jason <alexanderjason526@gmail.com>
2023-07-10 13:12:06 +0700

    feat: add web implementation

---
+++ src/main/java/com/nostratech/movie/web/HelloResources.java
@@ -0,0 +1,40 @@
+package com.nostratech.movie.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nostratech.movie.dto.HelloMessageResponseDTO;
+import com.nostratech.movie.service.GreetingService;
+
+
+@RestController
+public class HelloResources {
+	
+	Logger log = LoggerFactory.getLogger(HelloResources.class);
+
+	private GreetingService greetingService;
+	
+	public HelloResources(GreetingService greetingService) {
+		super();
+		this.greetingService = greetingService;
+	}
+
+	//POST, PUT, DELETE, OPTION, TRACE, HEAD, PATCH
+	@GetMapping("/hello")
+	public ResponseEntity<HelloMessageResponseDTO> helloWorld() {
+		log.trace("this is log TRACE");
+		log.debug("this is log DEBUG");
+		log.info("this is log INFO");
+		log.warn("this is log WARN");
+		log.error("this is log ERROR");
+		HelloMessageResponseDTO dto = new HelloMessageResponseDTO();
+		dto.setMessage(greetingService.sayGreeting());
+		return ResponseEntity.accepted().body(dto);
+	}
+	
+	
+}
--- src/main/java/com/nostratech/movie/web/MovieResource.java
+++ src/main/java/com/nostratech/movie/web/MovieResource.java
@@ -1,5 +1,73 @@
 package com.nostratech.movie.web;
 
-public class MovieResource {
+import java.net.URI;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nostratech.movie.dto.MovieDetailDTO;
+import com.nostratech.movie.dto.MovieCreateDTO;
+import com.nostratech.movie.dto.MovieUpdateRequestDTO;
+import com.nostratech.movie.service.MovieService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+public class MovieResource {
+	
+	private final MovieService MovieService;
+
+	//nama yang salah /get-Movie/{MovieId}
+	@GetMapping("/movie/{MovieId}")
+	public MovieDetailDTO findMovieDetail(@PathVariable("MovieId") Long id) {
+		StopWatch stopWatch = new StopWatch();
+		log.info("start findMovieDetail "+id);
+		stopWatch.start();
+		MovieDetailDTO result =  MovieService.findMovieDetailById(id);
+		log.info("finish findMovieDetail. execution time = {}",stopWatch.getTotalTimeMillis());
+		return result;
+
+	}
+	
+	//nama yang salah /save-Movie /create-Movie
+	@PostMapping("/movie")
+	public ResponseEntity<Void> createANewMovie(@RequestBody MovieCreateDTO dto){
+		MovieService.createNewMovie(dto);
+		return ResponseEntity.created(URI.create("/  ")).build();
+	}
+	
+	@GetMapping("/movie")
+	public ResponseEntity<List<MovieDetailDTO>> findMovieList(){
+		return ResponseEntity.ok().body(MovieService.findMovieListDetail());
+		
+	}
+	
+	//PUT /movie
+	@PutMapping("/movie/{MovieId}")
+	public ResponseEntity<Void> updateMovie(@PathVariable("MovieId") Long MovieId, 
+			@RequestBody MovieUpdateRequestDTO dto){
+		MovieService.updateMovie(MovieId, dto);
+		return ResponseEntity.ok().build();
+	}
+	
+	//DELETE /movie
+	@DeleteMapping("/movie/{MovieId}")
+	public ResponseEntity<Void> deleteMovie(@PathVariable("MovieId") Long MovieId){
+		MovieService.deleteMovie(MovieId);
+		return ResponseEntity.ok().build();
+	}
+	
 }
