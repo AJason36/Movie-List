febf3c409d50aad80150a79f900e92d5f68bdb9a
Alexander Jason <alexanderjason526@gmail.com>
2023-07-17 12:53:41 +0700

    chore: remove impl

--- src/main/java/com/nostratech/movie/repository/impl/MovieRepositoryImpl.java
+++ src/main/java/com/nostratech/movie/repository/impl/MovieRepositoryImpl.java
@@ -10,43 +10,6 @@ import com.nostratech.movie.repository.MovieRepository;
 import lombok.Data;
 
 @Data
-public class MovieRepositoryImpl implements MovieRepository{
-
-	private Map<Long, Movie> MovieMap;
-
-	
-	@Override
-	public Movie findMovieById(Long id) {
-		Movie movie = MovieMap.get(id);
-		return movie;
-	}
-
-
-	@Override
-	public List<Movie> findAll() {
-		List<Movie> MovieList = new ArrayList<Movie>(MovieMap.values());
-		return MovieList;
-	}
-
-
-	@Override
-	public void save(Movie Movie) {
-		int size = MovieMap.size();
-		Movie.setId((long) size+1);
-		MovieMap.put(Movie.getId(), Movie);
-	}
-
-
-	@Override
-	public void update(Movie Movie) {
-		MovieMap.put(Movie.getId(), Movie);	
-	}
-
-
-	@Override
-	public void delete(Long MovieId) {
-		MovieMap.remove(MovieId);
-		
-	}
+public class MovieRepositoryImpl{
 
 }
