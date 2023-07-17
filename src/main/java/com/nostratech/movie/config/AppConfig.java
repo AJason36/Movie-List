0b7ee25b56359f9ba399b3dc78c94494a46aa8cc
Alexander Jason <alexanderjason526@gmail.com>
2023-07-17 12:50:52 +0700

    chore: remove bean

--- src/main/java/com/nostratech/movie/config/AppConfig.java
+++ src/main/java/com/nostratech/movie/config/AppConfig.java
@@ -20,65 +20,65 @@ import com.nostratech.movie.repository.impl.MovieRepositoryImpl;
 @Configuration
 public class AppConfig {
 	
-	@Bean
-    public Movie movie( Review review1,  Review review2,   Actor actor1,  Actor actor2) {
-        Movie movie = new Movie();
-        movie.setId(1L);
-        movie.setTitle("Lost Treasure");
-        movie.setGenre("Adventure");
+	// @Bean
+    // public Movie movie( Review review1,  Review review2,   Actor actor1,  Actor actor2) {
+    //     Movie movie = new Movie();
+    //     movie.setId(1L);
+    //     movie.setTitle("Lost Treasure");
+    //     movie.setGenre("Adventure");
 
-        List<Review> reviews = new ArrayList<>();
-        reviews.add(review1);
-        reviews.add(review2);
+    //     List<Review> reviews = new ArrayList<>();
+    //     reviews.add(review1);
+    //     reviews.add(review2);
 
-        movie.setReviews(reviews);
+    //     movie.setReviews(reviews);
 
-        List<Actor> actors = new ArrayList<>();
-        actors.add(actor1);
-        actors.add(actor2);
+    //     List<Actor> actors = new ArrayList<>();
+    //     actors.add(actor1);
+    //     actors.add(actor2);
 
-        movie.setActors(actors);
+    //     movie.setActors(actors);
 
-        return movie;
-    }
+    //     return movie;
+    // }
 
-    @Bean
-    public Review review1() {
-    	Review review1 = new Review();
-        review1.setReview("abcd");
-        review1.setStar(4);
-        return review1;
-    }
+    // @Bean
+    // public Review review1() {
+    // 	Review review1 = new Review();
+    //     review1.setReview("abcd");
+    //     review1.setStar(4);
+    //     return review1;
+    // }
     
-    @Bean
-    public Review review2() {
-    	Review review2 = new Review();
-        review2.setReview("efgh");
-        review2.setStar(5);
-        return review2;
-    }
+    // @Bean
+    // public Review review2() {
+    // 	Review review2 = new Review();
+    //     review2.setReview("efgh");
+    //     review2.setStar(5);
+    //     return review2;
+    // }
 
-    @Bean
-    public Actor actor1() {
-    	Actor actor1 = new Actor();
-        actor1.setName("James Bond");
-        return actor1;
-    }
-    @Bean
-    public Actor actor2() {
-        Actor actor2 = new Actor();
-        actor2.setName("Jackie Chan");
-        return actor2;
-    }
+    // @Bean
+    // public Actor actor1() {
+    // 	Actor actor1 = new Actor();
+    //     actor1.setName("James Bond");
+    //     return actor1;
+    // }
+    // @Bean
+    // public Actor actor2() {
+    //     Actor actor2 = new Actor();
+    //     actor2.setName("Jackie Chan");
+    //     return actor2;
+    // }
     
-    @Bean
-	public MovieRepository MovieRepository(Movie movie) {
-		Map<Long, Movie> MovieMap = new HashMap<Long, Movie>();
-		MovieMap.put(1L, movie);		
-		MovieRepositoryImpl MovieRepository = new MovieRepositoryImpl();
-		MovieRepository.setMovieMap(MovieMap);
+    // @Bean
+	// public MovieRepository MovieRepository(Movie movie) {
+	// 	Map<Long, Movie> MovieMap = new HashMap<Long, Movie>();
+	// 	MovieMap.put(1L, movie);		
+	// 	MovieRepositoryImpl MovieRepository = new MovieRepositoryImpl();
+	// 	MovieRepository.setMovieMap(MovieMap);
 		
-		return MovieRepository;
-	}
+	// 	return MovieRepository;
+	// }
      
 }
