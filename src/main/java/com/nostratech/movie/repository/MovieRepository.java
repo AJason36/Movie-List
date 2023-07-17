34e0cfe768e36d8e9a8682211e5af37fbdda19c2
Alexander Jason <alexanderjason526@gmail.com>
2023-07-17 12:53:02 +0700

    feat: add optional function

--- src/main/java/com/nostratech/movie/repository/MovieRepository.java
+++ src/main/java/com/nostratech/movie/repository/MovieRepository.java
@@ -1,19 +1,23 @@
 package com.nostratech.movie.repository;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nostratech.movie.domain.Movie;
 
-public interface MovieRepository {
-	public Movie findMovieById(Long id);
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+	public Optional<Movie> findById(Long id);
+	public Optional<Movie> findBySecureId(String id);
 	
-	public List<Movie> findAll();
+	// public List<Movie> findAll();
 	
-	public void save(Movie movie);
+	// public void save(Movie movie);
 	
-	public void update(Movie movie);
+	// public void update(Movie movie);
 	
-	public void delete(Long movieId);
+	// public void delete(Long movieId);
 
 
 }
