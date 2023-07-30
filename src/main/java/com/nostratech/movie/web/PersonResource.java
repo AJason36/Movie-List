1f6801f088ba976a9929dabca4dc69519709c8d4
Alexander Jason <alexanderjason526@gmail.com>
2023-07-30 18:45:35 +0700

    feat: add delete person

--- src/main/java/com/nostratech/movie/web/PersonResource.java
+++ src/main/java/com/nostratech/movie/web/PersonResource.java
@@ -26,38 +26,12 @@ import lombok.AllArgsConstructor;
 @RestController
 public class PersonResource {
 	
-	private final PersonService PersonService;
+	private final PersonService personService;
 		
-	// //person detail
-	// @GetMapping("/v1/person/{id}/detail")
-	// public ResponseEntity<PersonResponseDTO> findPersonById(@PathVariable String id){
-	// 	return ResponseEntity.ok().body(PersonService.findPersonById(id));
-	// }
-	
-	// @PostMapping("/v1/person")
-	// public ResponseEntity<Void> createNewPerson(@RequestBody @Valid List<PersonCreateRequestDTO> dto){
-	// 	PersonService.createNewPerson(dto);
-	// 	return ResponseEntity.created(URI.create("/person")).build();
-	// }
-	
-	
-	// @PutMapping("/v1/person/{personId}")
-	// public ResponseEntity<Void> updatePerson(@PathVariable String personId, 
-	// 	 @RequestBody PersonUpdateRequestDTO dto){
-	// 	PersonService.updatePerson(personId, dto);
-	// 	return ResponseEntity.ok().build();
-	// }
-	
-	// @DeleteMapping("/v1/person/{personId}")
-	// public ResponseEntity<Void> deletePerson(@PathVariable String personId){
-	// 	PersonService.deletePerson(personId);
-	// 	return ResponseEntity.ok().build();
-	// }
 	@PostMapping("/v1/person")
 	public ResponseEntity<Void> createAndUpdateperson(@RequestBody PersonCreateRequestDTO dto){
-		PersonService.createAndUpdatePerson(dto);
-		return ResponseEntity.created(URI.create("/v1/person")).build();
-		
+		personService.createAndUpdatePerson(dto);
+		return ResponseEntity.created(URI.create("/v1/person")).build();	
 	}
 	
 	@GetMapping("/v1/person")
@@ -66,8 +40,14 @@ public class PersonResource {
 			@RequestParam(name = "limit", required = true, defaultValue = "10") Integer limit,
 			@RequestParam(name="sortBy", required = true, defaultValue = "name") String sortBy,
 			@RequestParam(name="direction", required = true, defaultValue = "asc") String direction,
-			@RequestParam(name="name", required = false) String personName){
-		return ResponseEntity.ok().body(PersonService.findPersonList(pages, limit, sortBy, direction, personName));
+			@RequestParam(name = "name", required = false) String personName) {
+		return ResponseEntity.ok().body(personService.findPersonList(pages, limit, sortBy, direction, personName));
+	}
+	
+	@DeleteMapping("/v1/person/{personId}")
+	public ResponseEntity<Void> deleteperson(@PathVariable("personId") String personId){
+		personService.deletePerson(personId);
+		return ResponseEntity.ok().build();
 	}
 
 }
