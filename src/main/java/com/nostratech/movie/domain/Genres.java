75ea620ab008645b9c904f24dde8beda20c0b7aa
Alexander Jason <alexanderjason526@gmail.com>
2023-07-31 13:43:49 +0700

    feat: add genre domain

---
+++ src/main/java/com/nostratech/movie/domain/Genres.java
@@ -0,0 +1,41 @@
+package com.nostratech.movie.domain;
+import java.util.List;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import lombok.Data;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "genres")
+// @DynamicUpdate
+@SQLDelete(sql = "UPDATE genres SET deleted = true WHERE id = ?")
+@Where(clause = "deleted=false")
+public class Genres {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "genre_generator")
+    @SequenceGenerator (name = "genre_generator", sequenceName = "genre_id_seq")
+    private Long id;
+    
+    @Column(name = "genre", unique=true, nullable=false)
+    private String genre;
+
+    @ManyToMany(mappedBy="genreList")
+    private List<Movie> moviesGenre;
+}
