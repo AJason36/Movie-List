03248188c0ac410b8848daab3e97db25ce4adc2b
Alexander Jason <alexanderjason526@gmail.com>
2023-08-09 14:47:11 +0700

    add jpql query

--- src/main/java/com/nostratech/movie/repository/MovieRepository.java
+++ src/main/java/com/nostratech/movie/repository/MovieRepository.java
@@ -6,19 +6,21 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.nostratech.movie.domain.Movie;
 
-public interface MovieRepository extends JpaRepository<Movie, Long>{
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 	public Optional<Movie> findById(Long id);
+
 	public Optional<Movie> findBySecureId(String id);
-	
+
 	// public List<Movie> findAll();
-	
+
 	// public void save(Movie movie);
-	
+
 	// public void update(Movie movie);
-	
+
 	// where id = :id AND deleted=false
 	public Optional<Movie> findByIdAndDeletedFalse(Long id);
 
@@ -27,4 +29,10 @@ public interface MovieRepository extends JpaRepository<Movie, Long>{
 	public List<Movie> findBySecureIdIn(List<String> movieIdList);
 
 	public Page<Movie> findByTitleLikeIgnoreCase(String movieTitle, Pageable pageable);
+
+	@Query("SELECT DISTINCT m FROM Movie m "
+			+ "INNER JOIN Genres g "
+			+ "WHERE LOWER(m.title) LIKE LOWER(CONCAT('%', :movieTitle, '%')) "
+			+ "AND LOWER(g.genre) LIKE LOWER(CONCAT('%', :genre, '%'))")
+	public Page<Movie> findMovieList(String movieTitle, String genre, Pageable pageable);
 }
