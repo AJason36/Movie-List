16a4ac4ef219aaa9818bbd9c5d6f9d0328bf12e0
Alexander Jason <alexanderjason526@gmail.com>
2023-08-14 14:34:48 +0700

    feat: jwt security

---
+++ src/main/java/com/nostratech/movie/dto/LoginRequestDTO.java
@@ -0,0 +1,5 @@
+package com.nostratech.movie.dto;
+
+public record LoginRequestDTO(String username, String password) {
+
+}
---
+++ src/main/java/com/nostratech/movie/util/TokenExtractor.java
@@ -0,0 +1,7 @@
+package com.nostratech.movie.util;
+
+public interface TokenExtractor {
+	
+	public String extract(String payload);
+
+}
